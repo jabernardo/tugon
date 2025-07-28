c23a4b8430f60d32e129e7adb23c87277f894e4d
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-07-28 20:30:28 +0800

    exclude swagger in prod

--- core/api.go
+++ core/api.go
@@ -3,6 +3,8 @@ package core
 import (
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	_ "github.com/jabernardo/tugon/docs"
 	httpSwagger "github.com/swaggo/http-swagger"
@@ -21,7 +23,19 @@ func New(router *Router, addr string) *API {
 }
 
 func (api *API) ListenAndServe() {
-	api.Router.Mux.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+	var (
+		environment string
+		exists      bool
+	)
+
+	if environment, exists = os.LookupEnv("ENV"); !exists {
+		Logger().Error("[core.api] Environment `ENV` is not defined")
+		os.Exit(1)
+	}
+
+	if strings.ToLower(environment) != "prod" {
+		api.Router.Mux.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+	}
 
 	Logger().Info("Running at", "addr", api.Addr)
 	err := http.ListenAndServe(api.Addr, &api.Router.Mux)
